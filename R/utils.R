f2cd9557642dcee042957c420872f07f1b998e9c
Claudio Zandonella <claudiozandonella@gmail.com>
2020-12-24 16:49:24 +0100

    redefine get_dribble and add get_path_dribble function to speed up searching id elements in drive

--- R/utils.R
+++ R/utils.R
@@ -64,8 +64,27 @@ check_identity <- function(file){
 #' @return A dribble object.
 #' @noRd
 #' @seealso [googledrive::dribble()]
-#'
+
 get_dribble <- function(gfile, path = NULL, team_drive = NULL) {
+  if (!is.null(path)) {
+    path_dribble <- get_path_dribble(path = path, team_drive = team_drive)
+    
+    googledrive::drive_find(q = c(paste0("'", path_dribble$id,"' in parents", collapse = " or "),
+                                  paste0("name = '", gfile,"'")),
+                            team_drive = team_drive)
+    
+  } else if (is.null(path) & !is.null(team_drive)) {
+    # TODO: revise this part according to team_drive functionality
+    googledrive::team_drive_get(team_drive) %>%
+      googledrive::drive_ls(pattern = gfile)
+  } else {
+    googledrive::drive_find(q = c("'root' in parents",
+                                  paste0("name = '", gfile,"'")),
+                            team_drive = team_drive)
+  }
+}
+
+get_dribble_old <- function(gfile, path = NULL, team_drive = NULL) {
   if (!is.null(path)) {
     googledrive::drive_get(path = path, team_drive = team_drive) %>%
       googledrive::drive_ls(pattern = gfile)
@@ -75,6 +94,42 @@ get_dribble <- function(gfile, path = NULL, team_drive = NULL) {
   } else {
     googledrive::drive_ls(path = "~", pattern = gfile)
   }
+}
+
+#' Get path dribble
+#'
+#' Gets dribble ("Drive tibble") for the last folder provided in the path
+#' starting from root.
+#'
+#' @inheritParams upload_rmd
+#' @return A dribble object. Note that multiple lines could be returned if
+#'   multiple folders  exist  with the same path.
+#' @noRd
+#' 
+get_path_dribble <- function(path , team_drive = NULL){
+  # get sequence of folders
+  path <- stringr::str_split(path, pattern = "/", simplify = TRUE)[1,]
+    
+  # get folders id starting from root
+  for (i in seq_along(path)){
+    id_folders <- if(i == 1L) "root" else dribble$id
+    
+    dribble <- googledrive::drive_find(
+      q = c(paste0("'", id_folders,"' in parents", collapse = " or "), 
+            "mimeType = 'application/vnd.google-apps.folder'",
+            paste0("name = '", path[i],"'")),
+      team_drive = team_drive)
+    
+    # Check if path is available on drive
+    if(nrow(dribble) < 1){
+      stop(paste0("Folder ", 
+                  sQuote(paste0(path[1:i], sep = "/",  collapse = "")),
+                  " does not exists in GoogleDrive."),
+           call. = FALSE)
+    }
+  }
+  
+  return(dribble)
 }
 
 #' Sanitize Rmd file downloaded from GoogleDrive
