d3a7baf04a53e748b31685c4240f205fe4f17c54
Claudio Zandonella <claudiozandonella@gmail.com>
2021-05-07 17:38:27 +0200

    Built site for trackdown: 0.0.0.9000@

---
+++ reference/figures/hex.R
@@ -0,0 +1,17 @@
+hexSticker::sticker(
+  package = "trackdown",                  # package name to display on sticker
+  p_size = 24,                          # size of package name
+  p_y = 1.5,                            # y of package name
+  p_color = "#00FF00",                  # color of package name
+  subplot = "trackdown.png",              # sticker feature
+  s_x = 1,                              # x of feature
+  s_y = .8,                             # y of feature
+  s_width = .48,                        # width of feature - maintains aspect ratio
+  h_size = 2,                           # border
+  h_color = "#00FF00",                  # color of border
+  h_fill = "white",                     # color of background
+  url = "github.com/Lingtax/trackdown",   # url at the bottom
+  u_color = "white",                    # color of url at the bottom
+  u_size = 3.5,                         # size of url at the bottom
+  filename = "trackdown_hex.png"          # location to save the image
+)
