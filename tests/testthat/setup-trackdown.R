b1321f7dfa64d6c42b095a04b7b61c848a664473
Claudio Zandonella <claudiozandonella@gmail.com>
2021-07-12 18:41:16 +0200

    fix issue path testthat

--- tests/testthat/setup-trackdown.R
+++ tests/testthat/setup-trackdown.R
@@ -3,8 +3,8 @@ library("vcr") # *Required* as vcr is set up on loading
 get_auth_credentials()
 
 invisible(vcr::vcr_configure(
-  dir = vcr::vcr_test_path("fixtures"),
+  dir = "../fixtures",
   filter_request_headers = list(Authorization = "My bearer token is safe"),
-  write_disk_path = vcr::vcr_test_path("vcr_files")
+  write_disk_path = "../vcr_files"
 ))
 vcr::check_cassette_names()
